571c0880ae7d219878dedc5d9bed1b2219520a98
Raffaele Mancuso <raffamaiden@gmail.com>
2019-04-26 11:51:02 +0200

    Added requirement

--- buildexe.bat
+++ buildexe.bat
@@ -1,2 +1,3 @@
-set PATH=C:\Windows\System32\downlevel;D:\WinPrograms\Python3;%PATH%;
+set PATH=C:\Windows\System32\downlevel;D:\WinPrograms\Python3;D:\WinPrograms\Python3\Scripts;%PATH%;
 pyinstaller --onefile --noupx eurosportplayer-dl.py
+pause
--- eurosportplayer-dl.py
+++ eurosportplayer-dl.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
     
+# From PyCryptodome
 from Crypto.Cipher import AES
 
 import copy
